ecbfb17d344e3cc731f07ea3f35695dda4e3a35f
Andrew <simon31@uwindsor.ca>
2023-11-02 21:24:02 -0400

    fakestreaming using a vector
    When you press a button it changes the picture, and if you resize the window it is always the correct size, the bitmaps are in a vector and being grabbed from that to simulate something being recieved kinda.

--- clientwindow/clientwindow.cpp
+++ clientwindow/clientwindow.cpp
@@ -5,6 +5,30 @@
 ClientWindow::ClientWindow(wxString title, std::string_view serverHostname, int serverPort)
 	: wxFrame(NULL, wxID_ANY, title), client{ Client{} }
 {
+
+	// Simulate receiving a stream of images
+
+	wxImage::AddHandler(new wxPNGHandler());
+	picture = true;
+
+	originalBitmap = wxBitmap(wxImage("../../../MNM.png", wxBITMAP_TYPE_PNG));
+	alternativeBitmap = wxBitmap(wxImage("../../../Sans_Shady.png", wxBITMAP_TYPE_PNG));
+
+	imageStream.push_back(originalBitmap);
+	imageStream.push_back(alternativeBitmap);
+
+	button = new wxButton(this, wxID_ANY, "Toggle Image", wxPoint(100, 100), wxDefaultSize);
+	button->Bind(wxEVT_BUTTON, &ClientWindow::OnButtonClick, this);
+
+	imageDisplay = new wxStaticBitmap(this, wxID_ANY, originalBitmap, wxDefaultPosition, wxDefaultSize);
+
+	wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
+
+	mainSizer->Add(button, 0, wxALIGN_CENTER | wxALL, 10);
+	mainSizer->Add(imageDisplay, 1, wxEXPAND | wxALL, 10);
+
+	SetSizer(mainSizer);
+
 	wxMenu* menuFile = new wxMenu;
 	menuFile->Append(ID_String, "&Send A String...\tCtrl-S", "Send a String to the Server");
 	menuFile->Append(ID_Number, "&Send A Number...\tCtrl-C", "Send a Number to the Server");
@@ -31,6 +55,8 @@ ClientWindow::ClientWindow(wxString title, std::string_view serverHostname, int 
 
 	Bind(wxEVT_MENU, &ClientWindow::OnAbout, this, wxID_ABOUT);
 	Bind(wxEVT_MENU, &ClientWindow::OnExit, this, wxID_EXIT);
+
+	Bind(wxEVT_SIZE, &ClientWindow::OnSize, this);
 
 	if (!client.Connect(serverHostname, serverPort)) {
 		wxLogFatalError("Failed to connect to server");
@@ -98,4 +124,39 @@ void ClientWindow::OnPNG(wxCommandEvent& event) {
 	else {
 		wxMessageBox("Sent PNG screenshot to the server", "StudentSync - Client", wxOK | wxICON_INFORMATION);
 	}
+}
+
+void ClientWindow::OnButtonClick(wxCommandEvent& event) {
+	picture = !picture;
+	newSize = GetClientSize();
+	UpdateImage(newSize);
+}
+
+void ClientWindow::OnSize(wxSizeEvent& event) {
+	newSize = GetClientSize();
+	UpdateImage(newSize);
+}
+
+void ClientWindow::UpdateImage(const wxSize& newSize) {
+	originalBitmap = imageStream.at(0);
+	alternativeBitmap = imageStream.at(1);
+
+
+	if (picture) {
+		originalBitmap = ResizeBitmap(originalBitmap, newSize);
+		currentBitmap = originalBitmap;
+	}
+	else {
+		alternativeBitmap = ResizeBitmap(alternativeBitmap, newSize);
+		currentBitmap = alternativeBitmap;
+	}
+
+	imageDisplay->SetBitmap(currentBitmap);
+
+}
+
+wxBitmap ClientWindow::ResizeBitmap(const wxBitmap& bitmap, const wxSize& newSize) {
+	wxImage image = bitmap.ConvertToImage();
+	image.Rescale(newSize.GetWidth(), newSize.GetHeight(), wxIMAGE_QUALITY_HIGH);
+	return wxBitmap(image);
 }
--- clientwindow/clientwindow.h
+++ clientwindow/clientwindow.h
@@ -12,6 +12,20 @@ public:
 private:
 	Client client;
 
+	bool picture;
+	wxSize newSize;
+	wxBitmap originalBitmap;
+	wxBitmap alternativeBitmap;
+	wxBitmap currentBitmap;
+	wxButton* button;
+	wxStaticBitmap* imageDisplay;
+	std::vector<wxBitmap> imageStream;
+	wxBitmap ResizeBitmap(const wxBitmap& bitmap, const wxSize& newSize);
+
+
+	void UpdateImage(const wxSize& newSize = wxDefaultSize);
+	void OnSize(wxSizeEvent& event);
+	void OnButtonClick(wxCommandEvent& event);
 	void OnString(wxCommandEvent& event);
 	void OnNumber(wxCommandEvent& event);
 	void OnPNG(wxCommandEvent& event);
